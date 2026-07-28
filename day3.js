fd3e58e20f859a4858cc8626d26d3794f0b1774f
2025B01010686 <saanvi.25b01010686@abes.ac.in>
2026-07-28 09:45:55 +0530

    Await

--- day3.js
+++ day3.js
@@ -1,22 +1,34 @@
 //promises for asynch
 //js single threaded
 
-const promiseOne = new Promise((resolve, reject)=>{
-    console.log("Promise task 1");
-    resolve("Promises passed by using resolve");
-    let msg=true;
-    if(!msg==true){
-        console.log("msg using promises failed")
-    }
-    else{
-        console.log("error........");
-    }
-    setTimeout(()=>{
-        console.log(resolve());
-    },2000)
-});
-promiseOne.then((result)=>{
-    console.log(result);
-}).catch((error)=>{
-    comsole.log(error);
-})
+//const promiseOne = new Promise((resolve, reject)=>{
+// new Promise((resolve, reject)=>{
+//     console.log("Promise task 1");
+//     resolve("Promises passed by using resolve");
+//     let msg=true;
+//     if(!msg==true){
+//         console.log("msg using promises failed")
+//     }
+//     else{
+//         console.log("error........");
+//     }
+//     setTimeout(()=>{
+//         console.log(resolve());
+//     },2000)
+// });
+// promiseOne.then((result)=>{
+//     console.log(result);
+// }).catch((error)=>{
+//     comsole.log(error);
+// })
+
+//Async await
+console.log("1");
+async function test(){
+    console.log("2");
+    console.log("3");
+    await console.log("3");
+    console.log("4");
+}
+test();
+console.log("5");
