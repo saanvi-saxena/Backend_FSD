f695bd00d1c9f1571d6d69c366b1d4e2f81838d0
Saanvi Saxena <saanvisaxena1015@gmail.com>
2026-08-02 18:29:10 +0530

    day3

--- day3.js
+++ day3.js
@@ -23,12 +23,51 @@
 // })
 
 //Async await
-console.log("1");
+// console.log("1");
+// async function test(){
+//     console.log("2");
+//     console.log("3");
+//     await console.log("3");
+//     console.log("4");
+// }
+// test();
+// console.log("5");
+
+// t1=test();
+// console.log("5");
+//create promises that will print username and password using resolve
+//and if username and password not found then it will call 
+//reject state and print Error...
+
+// new Promise((resolve, reject)=>{
+//     setTimeout(function(){
+//         console.log("this is promises")
+//         let err= true;
+//         if(!err){
+//             resolve("user: ABC, password:1234");
+//         }
+//         else{
+//             reject("Error: Data failed");
+//         }
+//     },2000);
+    
+// }).then(()=>{
+//     console.log();
+// }).catch((error)=>{
+//     console.log(error);
+// });
+
 async function test(){
-    console.log("2");
-    console.log("3");
-    await console.log("3");
-    console.log("4");
+    console.log("message: 1");
+    const response = await fetch("./student.json");
+    console.log(response.status)
+    const std = await response.json();
+    return std;
+    console.log("message: 3")
 }
-test();
-console.log("5");
+test().then((res)=>{
+    console.log(res);
+})
+.catch((error)=>{
+    console.error("Error fetching students data", error);
+})
