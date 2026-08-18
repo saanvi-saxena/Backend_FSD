26ef100a3456169784a2a5de9ee6f86d85fa3f89
Saanvi Saxena <saanvisaxena1015@gmail.com>
2026-08-18 09:21:27 +0530

    Events

--- Eventmodule/prg1.js
+++ Eventmodule/prg1.js
@@ -2,9 +2,14 @@
 //EventEmitter- on(emit event parameter, callback)- resgister event, event listener; emit(event parameter)- trigger/create/fire event
 
 const EventEmitter= require('events');
-const event = new EventEmitter();
-event.on("greet", ()=>{
-    console.log("This is event emitter");
+class MyEvent extends EventEmitter{}
+const events = new EventEmitter();
+events.on("greet", (name)=>{
+    console.log(`Hello, ${name}! This is event emitter`);  //template literal ${variable}
 
 })
-event.emit("greet");
+events.on("exit", ()=>{
+    console.log("This is exit event");
+});
+events.emit("greet","Saanvi");
+events.emit("exit");
